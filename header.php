d6fea9afbb9c5469f47e2173d1ae5445364625ff
Eli <eli.gaultney@gmail.com>
2021-04-03 19:03:27 +0700

    Initial upload
    Initialized with just a mock-up layout of the index page up

---
+++ header.php
@@ -0,0 +1,70 @@
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <!--Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    <!--Custom CSS -->
+    <link href="styles.css" rel="stylesheet">
+    <title>
+        <?php
+            //echo $page;
+            
+            switch ($page) {
+                case "default":
+                    echo "Gaultney Management LLC";
+                    break;
+                case "captionArchive":
+                    echo "POTW: Caption Archive";
+                    break;
+                case "captionSubmit":
+                    echo "POTW: Submit A Caption";
+                    break;
+                case "captionVote":
+                    echo "POTW: Vote On Your Favorite Caption";
+                    break;
+                case "CurrentProblems":
+                    echo "POTW: This Week's Math Problems";
+                    break;
+                case "LastWeekSolutions":
+                    echo "POTW: Last Week's Solutions";
+                    break;
+                case "MathArchive":
+                    echo "POTW: Problem and Solution Archive";
+                    break;
+            }
+        ?>
+    </title>
+</head>
+
+<body>
+
+<nav class="navbar navbar-expand-sm navbar-dark bg-info">
+    <div class="mx-auto d-sm-flex d-block flex-sm-nowrap">
+        <a class="navbar-brand" href="#">Gaultney Management LLC</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample11" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <!-- <div class="collapse navbar-collapse text-center" id="navbarsExample11">
+            <ul class="navbar-nav">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                </li>
+            </ul>
+        </div> -->
+    </div>
+</nav>
+
+<div class="container">
---
+++ index.php
@@ -0,0 +1,52 @@
+<!doctype html>
+
+<html lang="en">
+
+<?php 
+    $page = "default";
+    include 'header.php'; 
+?>
+
+    <!--First Row-->
+  <div class="row justify-content-center text-center m-4">
+    <div class="col-sm-4">
+        <div class="card bg-dark text-white">
+            <img class="card-img" src="200x200.svg" alt="Card image">
+            <div class="card-body">
+                <a href="application.php" class="stretched-link h5">Application</a>
+            </div>
+        </div>
+    </div>
+    <div class="col-sm-4">
+        <div class="card bg-dark text-white" >
+            <img class="card-img" src="200x200.svg" alt="Card image">
+            <div class="card-body">
+                <a href="application.php" class="stretched-link h5">Properties</a>
+            </div>
+        </div>  
+    </div>
+  </div>
+    <!-- Second Row -->
+  <div class="row justify-content-center text-center m-4">
+    <div class="col-sm-4">
+        <div class="card bg-dark text-white">
+            <img class="card-img" src="200x200.svg" alt="Card image">
+            <div class="card-body">
+                <a href="application.php" class="stretched-link h5">Submit A Ticket</a>
+            </div>
+        </div>
+    </div>
+    <div class="col-sm-4">
+        <div class="card bg-dark text-white">
+            <img class="card-img" src="200x200.svg" alt="Card image">
+            <div class="card-body">
+                <a href="application.php" class="stretched-link h5">FAQ</a>
+            </div>
+        </div>
+    </div>
+  </div>
+
+<?php 
+    include 'footer.php'; 
+?>
+</html>
