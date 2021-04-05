96ca5988f02ec698bf8ee64fe972c250be972cd0
Eli <eli@elis.space>
2021-04-05 15:21:04 +0700

    added application form page

---
+++ app.php
@@ -0,0 +1,17 @@
+<!doctype html>
+
+<html lang="en">
+
+<?php 
+    $page = "default";
+    include 'header.php'; 
+?>
+
+   <iframe class="airtable-embed" src="https://airtable.com/embed/shrMUhRNEnnrSTwpN?backgroundColor=red" frameborder="0" onmousewheel="" width="100%" height="533" style="background: transparent; border: 1px solid #ccc;"></iframe>
+
+<?php 
+    include 'footer.php'; 
+?>
+</html>
+
+
