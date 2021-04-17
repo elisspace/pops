ff83d12f68ca19ed6b4072a686506bbdb03e99e0
Eli <eli@elis.space>
2021-04-17 11:40:56 +0700

    Got picture carousels of properties all working

--- property.php
+++ property.php
@@ -12,17 +12,17 @@
     </h2>
     <div id="collapseOne" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
       <div class="accordion-body">
-        <!-- Shankle pics go here -->
 
-        <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+        <!-- Shankle pics go here -->
+        <div id="shankleCarousels" class="carousel slide" data-bs-ride="carousel">
           <div class="carousel-indicators">
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-label="Slide 4"></button>
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="4" aria-label="Slide 5"></button>
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="5" aria-label="Slide 6"></button>
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="6" aria-label="Slide 7"></button>
+            <button type="button" data-bs-target="#shankleCarousels" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#shankleCarousels" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#shankleCarousels" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            <button type="button" data-bs-target="#shankleCarousels" data-bs-slide-to="3" aria-label="Slide 4"></button>
+            <button type="button" data-bs-target="#shankleCarousels" data-bs-slide-to="4" aria-label="Slide 5"></button>
+            <button type="button" data-bs-target="#shankleCarousels" data-bs-slide-to="5" aria-label="Slide 6"></button>
+            <button type="button" data-bs-target="#shankleCarousels" data-bs-slide-to="6" aria-label="Slide 7"></button>
           </div>
           <div class="carousel-inner">
             <div class="carousel-item active">
@@ -47,16 +47,15 @@
               <img src="pics\shankle\signal-2021-04-09-011948_007.jpeg" class="d-block w-100" alt="alt text 7">
             </div>
           </div>
-          <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+          <button class="carousel-control-prev" type="button" data-bs-target="#shankleCarousels" data-bs-slide="prev">
             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
             <span class="visually-hidden">Previous</span>
           </button>
-          <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+          <button class="carousel-control-next" type="button" data-bs-target="#shankleCarousels" data-bs-slide="next">
             <span class="carousel-control-next-icon" aria-hidden="true"></span>
             <span class="visually-hidden">Next</span>
           </button>
         </div>
-
         <!-- end of Shankle pics -->
         <strong>Shankle Rd Apartment</strong> With 3 rooms, 2 bathrooms, a spiral staircase and a trap-door safe, you'll always have somewhere to hide on Shankle Road.
       </div>
@@ -70,7 +69,51 @@
     </h2>
     <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
       <div class="accordion-body">
-        <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+          <!-- Barn apt pics go here -->
+        <div id="barnAptCarousel" class="carousel slide" data-bs-ride="carousel">
+          <div class="carousel-indicators">
+            <button type="button" data-bs-target="#barnAptCarousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#barnAptCarousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#barnAptCarousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            <button type="button" data-bs-target="#barnAptCarousel" data-bs-slide-to="3" aria-label="Slide 4"></button>
+            <button type="button" data-bs-target="#barnAptCarousel" data-bs-slide-to="4" aria-label="Slide 5"></button>
+            <button type="button" data-bs-target="#barnAptCarousel" data-bs-slide-to="5" aria-label="Slide 6"></button>
+            <button type="button" data-bs-target="#barnAptCarousel" data-bs-slide-to="6" aria-label="Slide 7"></button>
+          </div>
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <img src="pics\barn\signal-2021-04-09-011913_001.jpeg" class="d-block w-100" alt="alt text 1">
+            </div>
+            <div class="carousel-item">
+              <img src="pics\barn\signal-2021-04-09-011913_002.jpeg" class="rounded mx-auto d-block vh-100" alt="alt text 2">
+            </div>
+            <div class="carousel-item">
+              <img src="pics\barn\signal-2021-04-09-011913_003.jpeg" class="rounded mx-auto d-block vh-100" alt="alt text 3">
+            </div>
+            <div class="carousel-item">
+              <img src="pics\barn\signal-2021-04-09-011913_004.jpeg" class="d-block w-100" alt="alt text 4">
+            </div>
+            <div class="carousel-item">
+              <img src="pics\barn\signal-2021-04-09-011913_005.jpeg" class="d-block w-100" alt="alt text 5">
+            </div>
+            <div class="carousel-item">
+              <img src="pics\barn\signal-2021-04-09-011913_006.jpeg" class="d-block w-100" alt="alt text 6">
+            </div>
+            <div class="carousel-item">
+              <img src="pics\barn\signal-2021-04-09-011913_007.jpeg" class="d-block w-100" alt="alt text 7">
+            </div>
+          </div>
+          <button class="carousel-control-prev" type="button" data-bs-target="#barnAptCarousel" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+          </button>
+          <button class="carousel-control-next" type="button" data-bs-target="#barnAptCarousel" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+          </button>
+        </div>
+        <!-- end of barn apt pics -->
+        <strong>Rosewood's Barn Apartment</strong>Ever wanted to live on an honest-to-God working cattle farm? Nothing will be more authentic than this cute apartment bordered by a barn (naturally) and green and clean pasture. Look out your window and enjoy watching deer frolic past happy cows, put on your boots, hop into your truck, and head to the Wal-Mart parking lot for another rolicking good night in the country. 
       </div>
     </div>
   </div>
@@ -82,7 +125,71 @@
     </h2>
     <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
       <div class="accordion-body">
-        <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+        <!-- Lot E pics go here -->
+        <div id="lotECarousel" class="carousel slide" data-bs-ride="carousel">
+          <div class="carousel-indicators">
+            <button type="button" data-bs-target="#lotECarousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#lotECarousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#lotECarousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            <button type="button" data-bs-target="#lotECarousel" data-bs-slide-to="3" aria-label="Slide 4"></button>
+            <button type="button" data-bs-target="#lotECarousel" data-bs-slide-to="4" aria-label="Slide 5"></button>
+            <button type="button" data-bs-target="#lotECarousel" data-bs-slide-to="5" aria-label="Slide 6"></button>
+            <button type="button" data-bs-target="#lotECarousel" data-bs-slide-to="6" aria-label="Slide 7"></button>
+            <button type="button" data-bs-target="#lotECarousel" data-bs-slide-to="7" aria-label="Slide 8"></button>
+            <button type="button" data-bs-target="#lotECarousel" data-bs-slide-to="8" aria-label="Slide 9"></button>
+            <button type="button" data-bs-target="#lotECarousel" data-bs-slide-to="9" aria-label="Slide 10"></button>
+            <button type="button" data-bs-target="#lotECarousel" data-bs-slide-to="10" aria-label="Slide 11"></button>
+            <button type="button" data-bs-target="#lotECarousel" data-bs-slide-to="11" aria-label="Slide 12"></button>
+          </div>
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <img src="pics\e\signal-2021-04-09-011711_001.jpeg" class="rounded mx-auto d-block" alt="alt text 1">
+              </div>
+            <div class="carousel-item">
+              <img src="pics\e\signal-2021-04-09-011711_002.jpeg" class="rounded mx-auto d-block" alt="alt text 2">
+            </div>
+            <div class="carousel-item">
+              <img src="pics\e\signal-2021-04-09-011711_003.jpeg" class="rounded mx-auto d-block" alt="alt text 3">
+            </div>
+            <div class="carousel-item">
+              <img src="pics\e\signal-2021-04-09-011711_004.jpeg" class="rounded mx-auto d-block" alt="alt text 4">
+            </div>
+            <div class="carousel-item">
+              <img src="pics\e\signal-2021-04-09-011711_005.jpeg" class="rounded mx-auto d-block" alt="alt text 5">
+            </div>
+            <div class="carousel-item">
+              <img src="pics\e\signal-2021-04-09-011711_006.jpeg" class="rounded mx-auto d-block" alt="alt text 6">
+            </div>
+            <div class="carousel-item">
+              <img src="pics\e\signal-2021-04-09-011711_007.jpeg" class="rounded mx-auto d-block" alt="alt text 7">
+            </div>
+            <div class="carousel-item">
+              <img src="pics\e\signal-2021-04-09-011711_008.jpeg" class="rounded mx-auto d-block" alt="alt text 8">
+            </div>
+            <div class="carousel-item">
+              <img src="pics\e\signal-2021-04-09-011711_009.jpeg" class="rounded mx-auto d-block" alt="alt text 9">
+            </div>
+            <div class="carousel-item">
+              <img src="pics\e\signal-2021-04-09-011711_010.jpeg" class="rounded mx-auto d-block" alt="alt text 10">
+            </div>
+            <div class="carousel-item">
+              <img src="pics\e\signal-2021-04-09-011711_011.jpeg" class="rounded mx-auto d-block" alt="alt text 11">
+            </div>
+            <div class="carousel-item">
+              <img src="pics\e\signal-2021-04-09-011711_012.jpeg" class="rounded mx-auto d-block" alt="alt text 12">
+            </div>
+          </div>
+          <button class="carousel-control-prev" type="button" data-bs-target="#lotECarousel" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+          </button>
+          <button class="carousel-control-next" type="button" data-bs-target="#lotECarousel" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+          </button>
+        </div>
+        <!-- end of lot E pics -->
+        <strong>Lot E Trailer</strong> An excellent example of an Earth-dwelling. Not experimental, but instead purely enjoyable. Extraordinary entertainment awaits! 
       </div>
     </div>
   </div>
@@ -94,6 +201,39 @@
     </h2>
     <div id="collapseFour" class="accordion-collapse collapse" aria-labelledby="headingFour" data-bs-parent="#accordionExample">
       <div class="accordion-body">
+        <!-- Lot B pics go here -->
+        <div id="lotBCarousel" class="carousel slide" data-bs-ride="carousel">
+          <div class="carousel-indicators">
+            <button type="button" data-bs-target="#lotBCarousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#lotBCarousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#lotBCarousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            <button type="button" data-bs-target="#lotBCarousel" data-bs-slide-to="3" aria-label="Slide 4"></button>
+            <button type="button" data-bs-target="#lotBCarousel" data-bs-slide-to="4" aria-label="Slide 5"></button>
+          </div>
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <img src="pics\b\signal-2021-04-09-011605_001.jpeg" class="d-block w-100" alt="alt text 1">
+            </div>
+            <div class="carousel-item">
+              <img src="pics\b\signal-2021-04-09-011605_002.jpeg" class="d-block w-100" alt="alt text 2">
+            </div>
+            <div class="carousel-item">
+              <img src="pics\b\combined.jpg" class="d-block w-100" alt="alt text 3">
+            </div>
+           <div class="carousel-item">
+              <img src="pics\b\signal-2021-04-09-011605_005.jpeg" class="d-block w-100" alt="alt text 4">
+            </div>
+          </div>
+          <button class="carousel-control-prev" type="button" data-bs-target="#lotBCarousel" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+          </button>
+          <button class="carousel-control-next" type="button" data-bs-target="#lotBCarousel" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+          </button>
+        </div>
+        <!-- end of lot E pics -->
         <strong>This is the fourth item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
       </div>
     </div>
