6ecfd371d6538fcb9cf1efa2a8afbd00913acd6f
Eli <eli@elis.space>
2021-05-04 09:34:48 +0700

    Replaced index image placeholders with flat icons

--- index.php
+++ index.php
@@ -6,16 +6,16 @@
     <!--First Row-->
   <div class="row justify-content-center text-center m-4">
     <div class="col-sm-4">
-        <div class="card bg-dark text-white">
-            <img class="card-img" src="200x200.svg" alt="Card image">
+        <div class="card bg-info text-white">
+            <img class="card-img" src="pics\icons\apply.jpg" alt="Card image">
             <div class="card-body">
                 <a href="app.php" class="stretched-link h5">Application</a>
             </div>
         </div>
     </div>
     <div class="col-sm-4">
-        <div class="card bg-dark text-white" >
-            <img class="card-img" src="200x200.svg" alt="Card image">
+        <div class="card bg-info text-success" >
+            <img class="card-img" src="pics\icons\house.jpg" alt="Card image">
             <div class="card-body">
                 <a href="property.php" class="stretched-link h5">Properties</a>
             </div>
@@ -25,16 +25,16 @@
     <!-- Second Row -->
   <div class="row justify-content-center text-center m-4">
     <div class="col-sm-4">
-        <div class="card bg-dark text-white">
-            <img class="card-img" src="200x200.svg" alt="Card image">
+        <div class="card bg-info text-secondary">
+            <img class="card-img" src="pics\icons\ticket.jpg" alt="Card image">
             <div class="card-body">
                 <a href="application.php" class="stretched-link h5">Submit A Ticket</a>
             </div>
         </div>
     </div>
     <div class="col-sm-4">
-        <div class="card bg-dark text-white">
-            <img class="card-img" src="200x200.svg" alt="Card image">
+        <div class="card bg-info text-warning">
+            <img class="card-img" src="pics\icons\faq.jpg" alt="Card image">
             <div class="card-body">
                 <a href="application.php" class="stretched-link h5">FAQ</a>
             </div>
