31d181e15d279da033a992760176b207a0fe7764
Eli <eli@elis.space>
2025-08-31 11:53:37 -0400

    fix rocksprings carousel

--- apply/p/rocksprings-b.php
+++ apply/p/rocksprings-b.php
@@ -3,90 +3,88 @@
     include '../header.php'; 
 ?>
 
-<h2><i>Photos Coming Soon!</i></h2>
-<h3><i>Under Renovation</i></h2>
 <!-- Start Carousel -->
 <div id="carousel-rocksprings-b" class="carousel slide" data-bs-ride="carousel">
   <div class="carousel-indicators">
-    <button type="button" data-bs-target="#carousel-rocksprings-b  " data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-    <button type="button" data-bs-target="#carousel-rocksprings-b " data-bs-slide-to="1" aria-label="Slide 2"></button> 
-    <button type="button" data-bs-target="#carousel-rocksprings-b " data-bs-slide-to="1" aria-label="Slide 3"></button> 
-    <button type="button" data-bs-target="#carousel-rocksprings-b " data-bs-slide-to="1" aria-label="Slide 4"></button> 
-    <button type="button" data-bs-target="#carousel-rocksprings-b " data-bs-slide-to="1" aria-label="Slide 5"></button> 
-    <button type="button" data-bs-target="#carousel-rocksprings-b " data-bs-slide-to="1" aria-label="Slide 6"></button> 
-    <button type="button" data-bs-target="#carousel-rocksprings-b " data-bs-slide-to="1" aria-label="Slide 7"></button> 
-    <button type="button" data-bs-target="#carousel-rocksprings-b " data-bs-slide-to="1" aria-label="Slide 8"></button> 
-    <button type="button" data-bs-target="#carousel-rocksprings-b " data-bs-slide-to="1" aria-label="Slide 9"></button> 
-    <button type="button" data-bs-target="#carousel-rocksprings-b " data-bs-slide-to="1" aria-label="Slide 10"></button> 
-    <button type="button" data-bs-target="#carousel-rocksprings-b " data-bs-slide-to="1" aria-label="Slide 11"></button> 
-    <button type="button" data-bs-target="#carousel-rocksprings-b " data-bs-slide-to="1" aria-label="Slide 12"></button> 
-    <button type="button" data-bs-target="#carousel-rocksprings-b " data-bs-slide-to="1" aria-label="Slide 13"></button> 
-    <button type="button" data-bs-target="#carousel-rocksprings-b " data-bs-slide-to="1" aria-label="Slide 14"></button> 
-    <button type="button" data-bs-target="#carousel-rocksprings-b " data-bs-slide-to="1" aria-label="Slide 15"></button> 
-    <button type="button" data-bs-target="#carousel-rocksprings-b " data-bs-slide-to="1" aria-label="Slide 16"></button> 
-    
+    <button type="button" data-bs-target="#carousel-rocksprings-b" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carousel-rocksprings-b" data-bs-slide-to="1" aria-label="Slide 2"></button> 
+    <button type="button" data-bs-target="#carousel-rocksprings-b" data-bs-slide-to="2" aria-label="Slide 3"></button> 
+    <button type="button" data-bs-target="#carousel-rocksprings-b" data-bs-slide-to="3" aria-label="Slide 4"></button> 
+    <button type="button" data-bs-target="#carousel-rocksprings-b" data-bs-slide-to="4" aria-label="Slide 5"></button> 
+    <button type="button" data-bs-target="#carousel-rocksprings-b" data-bs-slide-to="5" aria-label="Slide 6"></button> 
+    <button type="button" data-bs-target="#carousel-rocksprings-b" data-bs-slide-to="6" aria-label="Slide 7"></button> 
+    <button type="button" data-bs-target="#carousel-rocksprings-b" data-bs-slide-to="7" aria-label="Slide 8"></button> 
+    <button type="button" data-bs-target="#carousel-rocksprings-b" data-bs-slide-to="8" aria-label="Slide 9"></button> 
+    <button type="button" data-bs-target="#carousel-rocksprings-b" data-bs-slide-to="9" aria-label="Slide 10"></button> 
+    <button type="button" data-bs-target="#carousel-rocksprings-b" data-bs-slide-to="10" aria-label="Slide 11"></button> 
+    <button type="button" data-bs-target="#carousel-rocksprings-b" data-bs-slide-to="11" aria-label="Slide 12"></button> 
+    <button type="button" data-bs-target="#carousel-rocksprings-b" data-bs-slide-to="12" aria-label="Slide 13"></button> 
+    <button type="button" data-bs-target="#carousel-rocksprings-b" data-bs-slide-to="13" aria-label="Slide 14"></button> 
+    <button type="button" data-bs-target="#carousel-rocksprings-b" data-bs-slide-to="14" aria-label="Slide 15"></button> 
+    <button type="button" data-bs-target="#carousel-rocksprings-b" data-bs-slide-to="15" aria-label="Slide 16"></button> 
   </div>
+
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img src="/apply/pics/rocksprings/rocksprings-front-72-perc.webp" class="d-block w-100" alt="...">
+      <img src="/apply/pics/rocksprings/rocksprings-front-72-perc.webp" class="d-block img-fluid" alt="...">
     </div>  
     <div class="carousel-item active">
-      <img src="/apply/pics/rocksprings/rocksprings1.jpg" class="d-block w-100" alt="...">
+      <img src="/apply/pics/rocksprings/rocksprings1.jpg" class="d-block img-fluid" alt="...">
     </div>  
     <div class="carousel-item active">
-      <img src="/apply/pics/rocksprings/rocksprings2.jpg" class="d-block w-100" alt="...">
+      <img src="/apply/pics/rocksprings/rocksprings2.jpg" class="d-block img-fluid" alt="...">
     </div>  
     <div class="carousel-item active">
-      <img src="/apply/pics/rocksprings/rocksprings3.jpg" class="d-block w-100" alt="...">
+      <img src="/apply/pics/rocksprings/rocksprings3.jpg" class="d-block img-fluid" alt="...">
     </div>  
     <div class="carousel-item active">
-      <img src="/apply/pics/rocksprings/rocksprings4.jpg" class="d-block w-100" alt="...">
+      <img src="/apply/pics/rocksprings/rocksprings4.jpg" class="d-block img-fluid" alt="...">
     </div>  
     <div class="carousel-item active">
-      <img src="/apply/pics/rocksprings/rocksprings5.jpg" class="d-block w-100" alt="...">
+      <img src="/apply/pics/rocksprings/rocksprings5.jpg" class="d-block img-fluid" alt="...">
     </div>  
     <div class="carousel-item active">
-      <img src="/apply/pics/rocksprings/rocksprings6.jpg" class="d-block w-100" alt="...">
+      <img src="/apply/pics/rocksprings/rocksprings6.jpg" class="d-block img-fluid" alt="...">
     </div>  
     <div class="carousel-item active">
-      <img src="/apply/pics/rocksprings/rocksprings7.jpg" class="d-block w-100" alt="...">
+      <img src="/apply/pics/rocksprings/rocksprings7.jpg" class="d-block img-fluid" alt="...">
     </div>  
     <div class="carousel-item active">
-      <img src="/apply/pics/rocksprings/rocksprings8.jpg" class="d-block w-100" alt="...">
+      <img src="/apply/pics/rocksprings/rocksprings8.jpg" class="d-block img-fluid" alt="...">
     </div>  
     <div class="carousel-item active">
-      <img src="/apply/pics/rocksprings/rocksprings9.jpg" class="d-block w-100" alt="...">
+      <img src="/apply/pics/rocksprings/rocksprings9.jpg" class="d-block img-fluid" alt="...">
     </div>  
     <div class="carousel-item active">
-      <img src="/apply/pics/rocksprings/rocksprings10.jpg" class="d-block w-100" alt="...">
+      <img src="/apply/pics/rocksprings/rocksprings10.jpg" class="d-block img-fluid" alt="...">
     </div>  
     <div class="carousel-item active">
-      <img src="/apply/pics/rocksprings/rocksprings11.jpg" class="d-block w-100" alt="...">
+      <img src="/apply/pics/rocksprings/rocksprings11.jpg" class="d-block img-fluid" alt="...">
     </div>  
     <div class="carousel-item active">
-      <img src="/apply/pics/rocksprings/rocksprings12.jpg" class="d-block w-100" alt="...">
+      <img src="/apply/pics/rocksprings/rocksprings12.jpg" class="d-block img-fluid" alt="...">
     </div>  
     <div class="carousel-item active">
-      <img src="/apply/pics/rocksprings/rocksprings13.jpg" class="d-block w-100" alt="...">
+      <img src="/apply/pics/rocksprings/rocksprings13.jpg" class="d-block img-fluid" alt="...">
     </div>  
     <div class="carousel-item active">
-      <img src="/apply/pics/rocksprings/rocksprings14.jpg" class="d-block w-100" alt="...">
+      <img src="/apply/pics/rocksprings/rocksprings14.jpg" class="d-block img-fluid" alt="...">
     </div>  
     <div class="carousel-item active">
-      <img src="/apply/pics/rocksprings/rocksprings15.jpg" class="d-block w-100" alt="...">
+      <img src="/apply/pics/rocksprings/rocksprings15.jpg" class="d-block img-fluid" alt="...">
     </div>  
     <div class="carousel-item active">
-      <img src="/apply/pics/rocksprings/rocksprings16.jpg" class="d-block w-100" alt="...">
+      <img src="/apply/pics/rocksprings/rocksprings16.jpg" class="d-block img-fluid" alt="...">
     </div>  
     <!-- <div class="carousel-item active">
       <img src="/pics/d/15233.jpeg" class="d-block w-100" alt="...">
     </div> -->
   </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carousel-rocksprings-b  " data-bs-slide="prev">
+  <button class="carousel-control-prev" type="button" data-bs-target="#carousel-rocksprings-b" data-bs-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
     <span class="visually-hidden">Previous</span>
   </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carousel-rocksprings-b  " data-bs-slide="next">
+  <button class="carousel-control-next" type="button" data-bs-target="#carousel-rocksprings-b" data-bs-slide="next">
     <span class="carousel-control-next-icon" aria-hidden="true"></span>
     <span class="visually-hidden">Next</span>
   </button>
